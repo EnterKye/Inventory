5ed959d37ab885a00f169ab56e56b8f97936bed5
cn005897 <1098120843@qq.com>
2020-12-22 16:40:08 +0800

    dialog

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,10 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        Response.Write("<script>alert('欢迎光临')</script>");
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
 
     }
 }
